dd1dd7b1efa57210d15255ecd4c700a3f2e1b446
Salvatore Nalbone <salvatorenalbone.web@gmail.com>
2018-03-08 00:28:56 +0100

    mongo

--- src/Models/ModuleFieldTypes.php
+++ src/Models/ModuleFieldTypes.php
@@ -10,7 +10,7 @@
 namespace Dwij\Laraadmin\Models;
 
 use Jenssegers\Mongodb\Eloquent\Model;
-//use Illuminate\Database\Eloquent\Model; 
+//use Illuminate\Database\Eloquent\Model;
 
 class ModuleFieldTypes extends Model
 {
