1d8ab58f886c099563e9743cb5d283bd9139dfb9
Salvatore Nalbone <salvatorenalbone.web@gmail.com>
2018-03-07 19:55:18 +0100

    first commit

---
+++ src/Models/Menu.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Code generated using LaraAdmin
+ * Help: http://laraadmin.com
+ * LaraAdmin is open-sourced software licensed under the MIT license.
+ * Developed by: Dwij IT Solutions
+ * Developer Website: http://dwijitsolutions.com
+ */
+
+namespace Dwij\Laraadmin\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Dwij\Laraadmin\Helpers\LAHelper;
+
+/**
+ * Class Menu
+ * @package Dwij\Laraadmin\Models
+ *
+ * Menu Model which looks after Menus in Sidebar and Navbar
+ */
+class Menu extends Model
+{
+    protected $table = 'la_menus';
+    
+    protected $guarded = [
+    
+    ];
+}
